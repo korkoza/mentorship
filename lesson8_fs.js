816fb6b1a2f3cd110c43214b004466094036b8ba
Mykola Korkoza <mykola.korkoza@gmail.com>
2018-10-22 23:23:09 +0300

    lesson8

---
+++ lesson8_fs.js
@@ -0,0 +1,34 @@
+let fs = require('fs-extra');
+let randomstring = require('randomstring');
+let path = require('path');
+
+fs.open('newFile.txt','w', function(err, data) {
+    if(err) throw err;
+    console.log('saved');
+});
+
+fs.appendFile ('newFile.txt', 'Content created', function(err){
+    if (err) throw err;
+    console.log('updated');
+})
+
+fs.writeFile ('newFile.txt', randomstring.generate(20), function(err){
+    if (err) throw err;
+    console.log('changed');
+})
+
+fs.readFile ('newFile.txt', 'utf8', function(err, data){
+    if (err) throw err;
+    console.log(data);
+})
+
+
+// write file when folders don't exist
+
+    fs.outputFile('./folder1/folder2/file2.txt', 'folders are created as well', function(err){
+    if(err) throw err;
+    console.log('saved with folders as well');
+    })
+
+
+
---
+++ lesson8_json.js
@@ -0,0 +1,15 @@
+let student = {
+    "name": "Petro",
+    "surname": "Ivantsiv",
+    "rate": 4.6
+}
+
+console.log(student.name);
+
+student.rate = 4.7;
+
+obj = JSON.stringify(student);
+
+console.log(obj);
+
+
